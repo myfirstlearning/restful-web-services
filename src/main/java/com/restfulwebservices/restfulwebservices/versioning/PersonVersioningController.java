2bf47043f64c71e9ce8eff346cae6b63aeb28802
DESKTOP-95F3B96\dheeraj <dheerajb412@gmail.com>
2020-07-08 21:34:32 -0400

    versioning

---
+++ src/main/java/com/restfulwebservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -0,0 +1,58 @@
+package com.restfulwebservices.restfulwebservices.versioning;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonVersioningController {
+
+
+    //URI TYPE VERSIONING
+    @GetMapping("v1/person")
+    public PersonV1 personV1(){
+        return  new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping("v2/person")
+    public PersonV2 personV2(){
+        return new PersonV2(new Name("Bob","Charlie"));
+    }
+
+    //************** Params ****************
+    //PARAMS TYPE VERSIONING
+    @GetMapping(value="/person/param", params="version=1")
+    public PersonV1 paramV1(){
+        return  new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value="/person/param", params="version=2")
+    public PersonV2 paramV2(){
+        return new PersonV2(new Name("Bob","Charlie"));
+    }
+
+    //************* Headers ************
+    //HEADER TYPE VERSIONING
+    @GetMapping(value="/person/header", headers="X-API-VERSION=1")
+    public PersonV1 headerV1(){
+        return  new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value="/person/header", headers="X-API-VERSION=2")
+    public PersonV2 headerV2(){
+        return new PersonV2(new Name("Bob","Charlie"));
+    }
+
+    //*********** Produces ***********
+    //MIME TYPE VERSIONING/MEDIA TYPE/CONTENT NEGOTIATION/ACCEPT HEADER
+    @GetMapping(value="/person/produces", produces="application/vnd.company.app-v1+json")
+    public PersonV1 producesV1(){
+        return  new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value="/person/produces", produces="application/vnd.company.app-v2+json")
+    public PersonV2 producesV2(){
+        return new PersonV2(new Name("Bob","Charlie"));
+    }
+
+}
